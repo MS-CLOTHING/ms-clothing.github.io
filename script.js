11ad0e7b01c873d3b6b9601c1b4ca1c13cf5e521
ORION\mohan.ku <mohan.ku@orioninc.com>
2024-01-14 12:09:59 +0530

    added dom for details

---
+++ script.js
@@ -0,0 +1,36 @@
+let collections
+document.addEventListener('DOMContentLoaded', function() {
+    collections = document.querySelectorAll('.cloth-collections img')  ;
+    console.log(collections);
+    domActivity();
+    
+});
+
+
+function domActivity()
+{
+    for(let i=0;i<collections.length;i++)
+    {
+      collections[i].addEventListener('click',(e=>{
+        console.log(collections[i]);
+        collections[i].style.border ="1px solid black";
+        let  selectedImage =collections[i].getAttribute('src');
+        console.log(selectedImage);
+       let mainImg = this.document.querySelector('.main-img img');
+       console.log(mainImg);
+        mainImg.setAttribute('src',selectedImage);
+
+         collections.forEach((element) => {
+                 if(element != collections[i])
+                 {
+                   element.style.border ="none";
+                 }
+         });
+      }))
+    }
+}
+
+function directToWhatsapp()
+{
+    window.open(`https://api.whatsapp.com/send?phone=918220162220&text=Hai I Want Shop a Cloth ! `, '_blank');
+}
