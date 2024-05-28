c50f6b234ba6ed2cd5abe44a8507b00d6f9a6356
ORION\mohan.ku <mohan.ku@orioninc.com>
2024-05-28 07:16:14 +0530

    added navy and charcoal images

--- data.js
+++ data.js
@@ -4,11 +4,11 @@ tshirts = [{
     price:"250",
     mrp:"600",
     images:[
-      "./assets/images/black.jpg",
-       "./assets/images/black.jpg",
-       "./assets/images/black.jpg",
-       "./assets/images/black.jpg",
-       "./assets/images/black.jpg",
+      "./assets/images/Gray/gray-1.jpg",
+      "./assets/images/Gray/gray-3.jpg",
+      "./assets/images/Gray/gray-2.jpg",
+      "./assets/images/Gray/gray-4.jpg",
+      "./assets/images/Gray/gray-5.jpg",
     ],
     size:[
       'S-36','M-38','L-40','XL-42'
@@ -25,11 +25,32 @@ tshirts = [{
     price:"250",
     mrp:"600",
     images:[
-      "./assets/images/blue.png",
-      "./assets/images/blue.png",
-      "./assets/images/blue.png",
-      "./assets/images/blue.png",
-      "./assets/images/blue.png",
+      "./assets/images/Blue/blue-1.jpg",
+      "./assets/images/Blue/blue-3.jpg",
+      "./assets/images/Blue/blue-2.jpg",
+      "./assets/images/Blue/blue-4.jpg",
+      "./assets/images/Blue/blue-5.jpg",
+    ],
+    size:[
+      'S-36','M-38','L-40','XL-42'
+    ],
+    features:[
+      'Half Sleeve',
+      'Round Neck',
+      '100% Cotton',
+      'Label Free'
+    ]
+  },
+  {
+    name:"2 T-shirts Navy And Charcoal",
+    price:"400",
+    mrp:"1200",
+    images:[
+      "./assets/images/Blue/Combo-2.jpg",
+      "./assets/images/Blue/Combo-1.jpg",
+      "./assets/images/Gray/gray-4.jpg",
+      "./assets/images/Blue/blue-4.jpg",
+      "./assets/images/Blue/blue-5.jpg",
     ],
     size:[
       'S-36','M-38','L-40','XL-42'
